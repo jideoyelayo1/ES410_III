0ecff445c6a8a1be9ee478a77cbf02e53250243a
JideO <JideOyelayo1@Gmail.com>
2022-12-06 23:10:13 +0000

    Update

--- Sensors/Testing/NPK_temperature_humidity_Sensor/NPK_Plus_Temperature_Plus_Humidity/NPK_Plus_Temperature_Plus_Humidity.ino
+++ Sensors/Testing/NPK_temperature_humidity_Sensor/NPK_Plus_Temperature_Plus_Humidity/NPK_Plus_Temperature_Plus_Humidity.ino
@@ -2,16 +2,25 @@
 #include <SoftwareSerial.h>
 #include "DHT.h"
 
-// Pins being used
+/*Pins being used*/
+
+// Humidity and Temperature Pins
 #define DHTPIN 4
 #define DHTTYPE DHT11
+// NPK Sensor Pins
 #define RO 2
 #define RE 8
 #define DE 7
 #define DI 3
+
+// NPK sensor byteArrays
+const byte nitro[] = {0x01,0x03, 0x00, 0x1e, 0x00, 0x01, 0xe4, 0x0c};
+const byte phos[] = {0x01,0x03, 0x00, 0x1f, 0x00, 0x01, 0xb5, 0xcc};
+const byte pota[] = {0x01,0x03, 0x00, 0x20, 0x00, 0x01, 0x85, 0xc0};
+byte values[11];
+
 SoftwareSerial mod(RO,DI);
-
-
+DHT dht(DHTPIN, DHTTYPE);
 
 
 void setup() {
