f26dca90b671d909d27dd1b4ce27ac1b95561d47
JideO <JideOyelayo1@Gmail.com>
2022-12-06 23:05:58 +0000

    Update

--- Sensors/Testing/NPK_temperature_humidity_Sensor/NPK_Plus_Temperature_Plus_Humidity/NPK_Plus_Temperature_Plus_Humidity.ino
+++ Sensors/Testing/NPK_temperature_humidity_Sensor/NPK_Plus_Temperature_Plus_Humidity/NPK_Plus_Temperature_Plus_Humidity.ino
@@ -2,6 +2,17 @@
 #include <SoftwareSerial.h>
 #include "DHT.h"
 
+// Pins being used
+#define DHTPIN 4
+#define DHTTYPE DHT11
+#define RO 2
+#define RE 8
+#define DE 7
+#define DI 3
+SoftwareSerial mod(RO,DI);
+
+
+
 
 void setup() {
   // put your setup code here, to run once:
