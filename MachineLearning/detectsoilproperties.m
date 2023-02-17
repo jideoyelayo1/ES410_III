e136b972fad1d178bc5042372d25d946a4f41187
Jide Oyelayo <JideOyelayo1@Gmail.com>
2023-02-17 18:21:28 +0000

    file update

--- MachineLearning/detectsoilproperties.m
+++
@@ -1,5 +0,0 @@
-function fertilise = detectsoilproperties(temperature, humidity, nitrogen, phosphorus, potassium) %#codegen
-mdl = loadLearnerForCoder('TrainingDatasetv4.mat');
-predictorValues = [temperature, humidity, nitrogen, phosphorus, potassium];
-fertilise = predict(mdl,predictorValues);
-end
--- detectsoilproperties_decisiontree.m
+++
@@ -1,7 +0,0 @@
-load detectsoilproperties_data.mat % loads training dataset of temperature, humidity and NPK values as predictors, labels as the estimators
-
-ctree = fitctree(sensordata, labels); % creates decision tree based on training dataset
-view(ctree);
-view(ctree, 'Mode','graph'); % plots trained decision tree model
-
-saveLearnerForCoder(ctree,'TrainingDatasetv4'); % saves model for C-code generation
